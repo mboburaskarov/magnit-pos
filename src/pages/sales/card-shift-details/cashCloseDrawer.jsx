94e09b0f4c62fa3d4dc64e51ea6cec6aa5b14bdd
jsdevuz <jsdevuz.me@gmail.com>
2025-04-03 23:12:43 +0500

    roll back

--- src/pages/sales/card-shift-details/cashCloseDrawer.jsx
+++ src/pages/sales/card-shift-details/cashCloseDrawer.jsx
@@ -288,15 +288,9 @@ function CashCloseDrawer({ open, setOpen }) {
           <Button
             type='submit'
             onClick={() =>
-              // closeZReport({
-              //   token: 'DXJFX32CN1296678504F2', // Токен всегда равен DXJFX32CN1296678504F2, используется везде, Обязательное поле, String
-              //   method: 'closeZreport', // Название метода, Обязательное поле, String
-              // })
-              closeCheckZReport({
-                token: 'DXJFX32CN1296678504F2',
-                method: 'getZreportInfo',
-                printerSize: 80,
-                zReportId: 1,
+              closeZReport({
+                token: 'DXJFX32CN1296678504F2', // Токен всегда равен DXJFX32CN1296678504F2, используется везде, Обязательное поле, String
+                method: 'closeZreport', // Название метода, Обязательное поле, String
               })
             }
             sx={{ bottom: 0, margin: '0 24px 24px', '& > svg': { width: 24, height: 24, ml: '12px' } }}
