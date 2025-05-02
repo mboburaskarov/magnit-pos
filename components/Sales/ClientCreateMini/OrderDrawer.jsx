06a6ee5642c4ec836b97cf6089bf8dcae1a05c60
jsdevuz <jsdevuz.me@gmail.com>
2025-05-02 20:52:31 +0500

    fix cal tin in slae

--- components/Sales/ClientCreateMini/OrderDrawer.jsx
+++ components/Sales/ClientCreateMini/OrderDrawer.jsx
@@ -340,10 +340,10 @@ export default function OrderDrawer({
           return Object.values(markingsList[el.id] || {}).map((marking, index) => ({
             barcode: el.barcode,
             amount: el.quantity > index ? (el.quantity / el.quantity) * 1000 : el.unit_amount * 1000,
-            price: el.quantity > index ? el.unit_price * 100 : el.unit_quantity_price * el.unit_quantity * 100,
+            price: el.quantity > index ? Math.round(el.unit_price * 100) : Math.round(el.unit_quantity_price * el.unit_quantity * 100),
             discount: el.discount_amount,
             vatPercent: get(el, 'vat_percent'),
-            vat: el.quantity > index ? get(el, 'vat_price') * 100 : el.unit_vat_price * el.unit_quantity * 100,
+            vat: el.quantity > index ? Math.round(get(el, 'vat_price') * 100) : Math.round(el.unit_vat_price * el.unit_quantity * 100),
             label: marking,
             name: el.name,
             classCode: get(el, 'class_code'),
@@ -369,10 +369,12 @@ export default function OrderDrawer({
             clientName: get(customerId, 'name'), //ФИО Клиента
 
             items: mockData.flat(),
-            receivedCash:
-              mpaddedPaymentsList.filter((item) => !item.isPlaceholder && item.type === 'cash').reduce((sum, item) => sum + (item.amount || 0), 0) * 100, // Сумма полученной наличности. Значение указывается в тийинах (100 сум = 10000 тийин)
-            receivedCard:
-              mpaddedPaymentsList.filter((item) => !item.isPlaceholder && item.type !== 'cash').reduce((sum, item) => sum + (item.amount || 0), 0) * 100, // Сумма полученной безналичности. Значение указывается в тийинах (100 сум = 10000 тийин)
+            receivedCash: Math.round(
+              paymentsList.filter((item) => item.amount && item.type === 'cash').reduce((sum, item) => sum + (item.amount || 0), 0) * 100
+            ), // Сумма полученной наличности. Значение указывается в тийинах (100 сум = 10000 тийин)
+            receivedCard: Math.round(
+              paymentsList.filter((item) => item.amount && item.type !== 'cash').reduce((sum, item) => sum + (item.amount || 0), 0) * 100
+            ), // Сумма полученной безналичности. Значение указывается в тийинах (100 сум = 10000 тийин)
           },
 
           ...(SALE_TYPE === 'RETURN' && {
