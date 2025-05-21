ce3ca826650be5286348baa261d676ec06963468
jsdevuz <jsdevuz.me@gmail.com>
2025-05-21 10:42:43 +0500

    remove uzum visa and blance payment types column from all-sales table

--- src/redux-toolkit/store.js
+++ src/redux-toolkit/store.js
@@ -40,7 +40,7 @@ import { userSlice } from './userSlice'
 // Define your migration function here
 const migrations = {
   // Example migration
-  270: (state) => {
+  271: (state) => {
     // Check if state needs migration
     if (!state.migrated) {
       // Perform migration logic
@@ -58,7 +58,7 @@ const migrations = {
 const persistConfig = {
   key: 'root',
   storage,
-  version: 270, // Current version of the persisted state
+  version: 271, // Current version of the persisted state
   migrate: (state) => {
     // Apply migrations based on state version
     return migrations[state._persist.version](state)
