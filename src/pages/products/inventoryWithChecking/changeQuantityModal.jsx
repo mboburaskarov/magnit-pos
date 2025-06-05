82cc678edeccd5a1e54cceabded4959678202484
jsdevuz <jsdevuz.me@gmail.com>
2025-06-05 12:47:03 +0500

    change ref to state inventory quantity change

--- src/pages/products/inventoryWithChecking/changeQuantityModal.jsx
+++ src/pages/products/inventoryWithChecking/changeQuantityModal.jsx
@@ -1,7 +1,7 @@
 import { Box, TextField, Typography } from '@mui/material'
 import { useTheme } from '@mui/styles'
 import { get } from 'lodash'
-import { useEffect, useRef } from 'react'
+import { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { useHotkeys } from 'react-hotkeys-hook'
 import { useTranslation } from 'react-i18next'
@@ -13,24 +13,29 @@ import { error } from '../../../../utils/toast'
 import errorAudio from '../../../assets/audio/error.mp3'
 import successAudio from '../../../assets/audio/normal.mp3'
 import CloseIcon from '../../../assets/icons/CloseIcon'
+
 export default function ChangeQuantityModal({ open, setBarcode, refetch, setOpen }) {
   const methods = useForm()
-  const { reset, control } = methods
+  const { reset } = methods
+  const { id } = useParams()
+  const theme = useTheme()
+  const { t } = useTranslation()
+
   const errorScanAudio = new Audio(errorAudio)
   const successScanAudio = new Audio(successAudio)
-  const qtyRef = useRef([])
-  const { id } = useParams()
 
-  const { mutate: setScanedNumber, isLoading: isSetScannedNumber } = useMutation(requests.sendScannedInventoryNumber, {
+  const [factQuantity, setFactQuantity] = useState('')
+  const [factUnit, setFactUnit] = useState('')
+  const [factQuantityRef, setFactQuantityRef] = useState(null)
+  const [factUnitRef, setFactUnitRef] = useState(null)
+
+  const { mutate: setScanedNumber } = useMutation(requests.sendScannedInventoryNumber, {
     onSuccess: ({ data }) => {
       refetch()
       setOpen(false)
       successScanAudio.play()
-      // setBarcode('')
-      // fetchStatusCountList()
-      // setBarcode('')g
     },
-    onError: (err) => {
+    onError: () => {
       refetch()
       errorScanAudio.play()
       error('Ошибка при сканирование!')
@@ -40,35 +45,32 @@ export default function ChangeQuantityModal({ open, setBarcode, refetch, setOpen
   useEffect(() => {
     reset({}, { keepDirty: true })
     if (open) {
+      setFactQuantity('')
+      setFactUnit('')
       setTimeout(() => {
-        qtyRef.current?.[0]?.focus()
+        factQuantityRef?.focus()
       }, 200)
     }
   }, [open])
-  const theme = useTheme()
 
   useHotkeys(
     '*',
     (event) => {
       if (event.code === 'NumpadSubtract' || event.code === 'NumpadAdd' || event.code === 'ShiftRight') {
-        qtyRef.current[1].focus()
+        factUnitRef?.focus()
       }
       if (event.code === 'Enter' || event.code === 'NumpadEnter') {
-        if (Number(qtyRef.current[0].value) == 0 && Number(qtyRef.current[1].value) == 0) {
+        if (Number(factQuantity) === 0 && Number(factUnit) === 0) {
           setOpen(false)
-          // setBarcode('')
           return
         }
+
         setScanedNumber({
           id,
           product_id: get(open, 'data.id'),
           type: 'MANUAL',
-          fact_quantity: Number(qtyRef.current[0].value),
-          fact_unit: Number(qtyRef.current[1].value),
-          // barcode: qtyRef.current[2].value,
-          // retail_price: Number(qtyRef.current[4].value),
-
-          // expire_date: qtyRef.current[3].value,
+          fact_quantity: Number(factQuantity),
+          fact_unit: Number(factUnit),
         })
       }
     },
@@ -78,7 +80,7 @@ export default function ChangeQuantityModal({ open, setBarcode, refetch, setOpen
       enableOnTags: ['INPUT', 'TEXTAREA'],
     }
   )
-  const { t } = useTranslation()
+
   return (
     <StyledEmptyDialog
       overflowVisible
@@ -105,176 +107,39 @@ export default function ChangeQuantityModal({ open, setBarcode, refetch, setOpen
         }}
       >
         <Typography sx={{ m: 'auto', width: '100%', textAlign: 'center', mb: '20px', fontWeight: '600' }}>{get(open, 'data.name')}</Typography>
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-          }}
-        >
-          <Box
-            sx={{
-              display: 'flex',
-              mb: '20px',
-              width: '100%',
-              justifyContent: 'space-between',
-            }}
-          >
+
+        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+          <Box sx={{ display: 'flex', mb: '20px', justifyContent: 'space-between' }}>
             <Box>
               <Typography sx={{ fontSize: 14, fontWeight: 600 }}>Факт УП</Typography>
               <TextField
+                type='number'
+                name='pack'
+                value={factQuantity}
+                onChange={(e) => setFactQuantity(e.target.value)}
+                inputRef={(ref) => setFactQuantityRef(ref)}
                 onKeyDown={(e) => {
                   const invalidKeys = ['e', 'E', '+', '-']
-
-                  if (invalidKeys.includes(e.key)) {
-                    e.preventDefault()
-                    return
-                  }
+                  if (invalidKeys.includes(e.key)) e.preventDefault()
                 }}
-                // onFocus={(e) => {
-                //   qtyRef.current[0].value = ''
-                // }}
-                // onBlur={(e) => {
-                //   if (e.target.value == '') {
-                //     qtyRef.current[0].value = get(open, 'data.fact_quantity')
-                //   }
-                // }}
-                // defaultValue={get(open, 'data.fact_quantity')}
-                name='pack'
-                inputRef={(e) => (qtyRef.current[0] = e)}
-                type='number'
               />
             </Box>
+
             <Box>
               <Typography sx={{ fontSize: 14, fontWeight: 600 }}>Факт кол-во</Typography>
-
               <TextField
-                // onFocus={(e) => {
-                //   qtyRef.current[1].value = ''
-                // }}
-                onKeyDown={(e) => {
-                  const invalidKeys = ['e', 'E', '+', '-']
-
-                  if (invalidKeys.includes(e.key)) {
-                    e.preventDefault()
-                    return
-                  }
-                }}
-                // onBlur={(e) => {
-                //   if (e.target.value == '') {
-                //     qtyRef.current[1].value = get(open, 'data.fact_unit')
-                //   }
-                // }}
-                // defaultValue={get(open, 'data.fact_unit')}
+                type='number'
                 name='unit'
-                inputRef={(e) => (qtyRef.current[1] = e)}
-                type='number'
+                value={factUnit}
+                onChange={(e) => setFactUnit(e.target.value)}
+                inputRef={(ref) => setFactUnitRef(ref)}
+                onKeyDown={(e) => {
+                  const invalidKeys = ['e', 'E', '+', '-']
+                  if (invalidKeys.includes(e.key)) e.preventDefault()
+                }}
               />
             </Box>
           </Box>
-          {/* <Box
-            sx={{
-              display: 'flex',
-              mb: '20px',
-              width: '100%',
-              justifyContent: 'space-between',
-            }}
-          > */}
-          {/* <Box
-              sx={{
-                width: '100%',
-              }}
-            >
-              <Typography sx={{ fontSize: 14, fontWeight: 600 }}>Штрих-код</Typography>
-
-              <TextField
-                // onFocus={(e) => {
-                //   qtyRef.current[2].value = ''
-                // }}
-                // onBlur={(e) => {
-                //   if (e.target.value == '') {
-                //     qtyRef.current[2].value = get(open, 'data.barcode')
-                //   }
-                // }}
-                onKeyDown={(e) => {
-                  const invalidKeys = ['e', 'E', '+', '-']
-
-                  if (invalidKeys.includes(e.key)) {
-                    e.preventDefault()
-                    return
-                  }
-                }}
-                sx={{
-                  width: '100%',
-                }}
-                defaultValue={get(open, 'data.barcode')}
-                name='barcode'
-                inputRef={(e) => (qtyRef.current[2] = e)}
-                type='number'
-              />
-            </Box>
-          </Box>
-
-          <Box
-            sx={{
-              display: 'flex',
-              mb: '20px',
-              width: '100%',
-              justifyContent: 'space-between',
-            }}
-          >
-            <Box>
-              <Typography sx={{ fontSize: 14, fontWeight: 600 }}>Цена</Typography>
-
-              <TextField
-                // onFocus={(e) => {
-                //   qtyRef.current[4].value = ''
-                // }}
-                // onBlur={(e) => {
-                //   if (e.target.value == '') {
-                //     qtyRef.current[4].value = get(open, 'data.retail_price')
-                //   }
-                // }}
-                onKeyDown={(e) => {
-                  const invalidKeys = ['e', 'E', '+', '-']
-
-                  if (invalidKeys.includes(e.key)) {
-                    e.preventDefault()
-                    return
-                  }
-                }}
-                defaultValue={get(open, 'data.retail_price')}
-                name='retail_price'
-                inputRef={(e) => (qtyRef.current[4] = e)}
-                type='number'
-              />
-            </Box>
-            <Box>
-              <Typography sx={{ fontSize: 14, fontWeight: 600 }}>Срок</Typography>
-
-              <TextField
-                // onFocus={(e) => {
-                //   qtyRef.current[3].value = ''
-                // }}
-                // onBlur={(e) => {
-                //   if (e.target.value == '') {
-                //     qtyRef.current[3].value = get(open, 'data.expire_date')
-                //   }
-                // }}
-                onKeyDown={(e) => {
-                  const invalidKeys = ['e', 'E', '+', '-']
-
-                  if (invalidKeys.includes(e.key)) {
-                    e.preventDefault()
-                    return
-                  }
-                }}
-                defaultValue={dayjs(get(open, 'data.expire_date')).format('YYYY-MM-DD')}
-                name='date'
-                inputRef={(e) => (qtyRef.current[3] = e)}
-                type='date'
-              />
-            </Box>
-          </Box> */}
         </Box>
       </Box>
     </StyledEmptyDialog>
