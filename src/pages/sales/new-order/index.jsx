909816a24626e198f27ee36557681dabc4e6e9eb
jsdevuz <jsdevuz.me@gmail.com>
2025-07-02 20:10:21 +0500

    add noor

--- src/pages/sales/new-order/index.jsx
+++ src/pages/sales/new-order/index.jsx
@@ -971,7 +971,7 @@ function NewSale() {
                   </Box>
                 </Box>
                 <Box display={'flex'}>
-                  {/* <ListItem sx={{ mr: '20px' }} className={`${classes.currentUser} drawer_user_avatar`} id='avatar' onClick={() => setIsOpenNoorDrawer(true)}>
+                  <ListItem sx={{ mr: '20px' }} className={`${classes.currentUser} drawer_user_avatar`} id='avatar' onClick={() => setIsOpenNoorDrawer(true)}>
                     <Box width={'100%'} display='flex' alignItems='center' justifyContent='space-between'>
                       <Box display={'flex'} justifyContent={'center'} flexDirection={'column'}>
                         <Typography id='user-username' className={classes.username}>
@@ -1019,7 +1019,7 @@ function NewSale() {
                         </Box>
                       </Box>
                     </Box>
-                  </ListItem> */}
+                  </ListItem>
                   <ListItem className={`${classes.currentUser} drawer_user_avatar`} id='avatar' onClick={() => setIsUserOpen(userData)}>
                     <Box width={'100%'} display='flex' alignItems='center' justifyContent='space-between'>
                       <Box display={'flex'} justifyContent={'center'} flexDirection={'column'}>
