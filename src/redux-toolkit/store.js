32e209bf9af1bf8bbf15360e5e7f03337fbbb7a3
jsdevuz <jsdevuz.me@gmail.com>
2025-07-22 14:21:48 +0500

    barcha sotuvlarda sana va vaqt alohida ustunlarda korinadigon qilindi

--- src/redux-toolkit/store.js
+++ src/redux-toolkit/store.js
@@ -49,7 +49,7 @@ import { userSlice } from './userSlice'
 // Define your migration function here
 const migrations = {
   // Example migration
-  356: (state) => {
+  357: (state) => {
     // Check if state needs migration
     if (!state.migrated) {
       // Perform migration logic
@@ -67,7 +67,7 @@ const migrations = {
 const persistConfig = {
   key: 'root',
   storage,
-  version: 356, // Current version of the persisted state
+  version: 357, // Current version of the persisted state
   migrate: (state) => {
     // Apply migrations based on state version
     return migrations[state._persist.version](state)
