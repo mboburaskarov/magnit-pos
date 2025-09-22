5083b9b678c1797c5d773bb9a07cbae4bbbb934d
jsdevuz <jsdevuz.me@gmail.com>
2025-09-22 12:44:43 +0500

    fix

--- utils/hasAccess.js
+++ utils/hasAccess.js
@@ -8,6 +8,8 @@ const hasAccess = (id, user_data) => {
   if (user_data?.type === 'SUPERADMIN') {
     return true
   }
+  console.log(user_data?.role_actions, id, has_access)
+
   return has_access ? true : false
 }
 
