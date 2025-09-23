9bca432057ac05a9c82233f9d16281008c835775
jsdevuz <jsdevuz.me@gmail.com>
2025-09-23 16:33:39 +0500

    chage api epos flesh cheking

--- src/pages/sales/new-order/index.jsx
+++ src/pages/sales/new-order/index.jsx
@@ -977,7 +977,7 @@ function NewSale() {
     onError: (err) => {
       setIsOrderDrower(false)
       setLiteOrder(false)
-      error('Ошибка EPOS getZreportInfo')
+      error('Ошибка EPOS getZReportsStatus')
       console.log('err', err)
     },
   })
@@ -985,7 +985,7 @@ function NewSale() {
     if (isOrderDrower || liteOrder) {
       checkEposFlesh({
         token: 'DXJFX32CN1296678504F2',
-        method: 'getZreportInfo',
+        method: 'getZReportsStatus',
         printerSize: 80,
         zReportId: 1,
       })
