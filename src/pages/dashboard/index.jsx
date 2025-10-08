197c03758ba44be00e8b62c1e78ca54f1aee39af
jsdevuz <jsdevuz.me@gmail.com>
2025-10-08 18:02:59 +0500

    fix franchise role

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -247,7 +247,7 @@ export default function DashboarPage() {
                     </Grid>
                   </CheckAccess>
                 ))}
-              <CheckAccess id={`farchise-dashboard-box`}>
+              <CheckAccess id={`franchise-dashboard-box`}>
                 <Grid item xs={12} xl={4} sm={12} md={6} lg={4} gap={0} pb={'0px'} pt={'20px !important'} spacing={2}>
                   <Box
                     onClick={() => navigate('/dashboard/b2b')}
