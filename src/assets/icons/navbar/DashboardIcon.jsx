68342d2b9ff9def63cbcf112b7aeda1b6a52768e
jsdevuz <jsdevuz.me@gmail.com>
2025-11-11 11:28:20 +0500

    fix transfer bug

--- src/assets/icons/navbar/DashboardIcon.jsx
+++ src/assets/icons/navbar/DashboardIcon.jsx
@@ -1,5 +1,5 @@
 const DashboardIcon = ({ color = '#FF6018' }) => (
-  <svg width='10' height='20' viewBox='0 0 20 20' fill='none' xmlns='http://www.w3.org/2000/svg'>
+  <svg width='20' height='20' viewBox='0 0 20 20' fill='none' xmlns='http://www.w3.org/2000/svg'>
     <path
       fillRule='evenodd'
       className='fill-icon'
