4608c28b59bbcf6ee4f0d17396b58463108b96b3
jsdevuz <jsdevuz.me@gmail.com>
2026-02-10 17:18:55 +0500

    fix: improve end date handling in imports list filter

--- src/pages/products/import-all/importList/index.jsx
+++ src/pages/products/import-all/importList/index.jsx
@@ -56,8 +56,11 @@ export default function ImportPage() {
       store_id: values?.store_id,
       start_date: values?.start_date ? values?.start_date + 'T00:00:00+05:00' : values?.start_date,
       end_date:
-        values?.end_date && values?.end_date == values?.start_date ? values?.end_date + 'T23:59:59+05:00' : values?.end_date + 'T23:59:59+05:00' || undefined,
-
+        values?.end_date && values?.end_date == values?.start_date
+          ? values?.end_date + 'T23:59:59+05:00'
+          : values?.end_date
+            ? values?.end_date + 'T23:59:59+05:00'
+            : undefined,
       status: values?.status,
       import_date: values?.import_date,
       received_amount_to: values?.received_amount_to,
