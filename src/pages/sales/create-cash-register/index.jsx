c7aa5e3014dfbc4c23c14441445a1bd8c48f78b6
jsdevuz <jsdevuz.me@gmail.com>
2026-04-07 21:48:37 +0500

    fix

--- src/pages/sales/create-cash-register/index.jsx
+++ src/pages/sales/create-cash-register/index.jsx
@@ -226,7 +226,7 @@ function NewCashRegister() {
   const { mutate: closeCheckZReport,isLoading: iscloseCheckZReport } = useMutation(requests.closeCheckZReport, {
     onSuccess: ({ data }) => {
        const terminalID = Object.keys(data?.message?.Sender?.ZReportFilesSent)[0]
-        if (!userData?.store?.terminal_ids.includes(terminalID||0) && hasAccess('check-terminal-id', userData)) {
+        if (!userData?.store?.terminal_ids?.includes(terminalID||0) && hasAccess('check-terminal-id', userData)) {
           setisEposTurnOn({ is_open: false, message: 'Вы в другом филиале!' })
           return
         } 
